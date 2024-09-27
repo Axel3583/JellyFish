dde85504d7d815ad6d95bd7a6e46f92f6f331204
Axel  Ibo <85737419+Axel3583@users.noreply.github.com>
2024-09-27 14:57:57 +0200

    Update App.test.tsx

--- src/Tests/App.test.tsx
+++ src/Tests/App.test.tsx
@@ -1,36 +1,36 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import App from './App';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
 
-// describe('App', () => {
-//   it('renders the search bar', () => {
-//     render(<App />);
+describe('App', () => {
+ it('renders the search bar', () => {
+     render(<App />);
 
-//     const searchBar = screen.getByPlaceholderText('Search cities...');
-//     expect(searchBar).toBeInTheDocument();
-//   });
+    const searchBar = screen.getByPlaceholderText('Search cities...');
+    expect(searchBar).toBeInTheDocument();
+   });
 
-//   it('displays city suggestions when typing in the search bar', () => {
-//     render(<App />);
+   it('displays city suggestions when typing in the search bar', () => {
+     render(<App />);
 
-//     const searchBar = screen.getByPlaceholderText('Search cities...');
+     const searchBar = screen.getByPlaceholderText('Search cities...');
 
-//     userEvent.type(searchBar, 'New York');
-//     const suggestionItem = screen.getByText('New York, USA');
-//     expect(suggestionItem).toBeInTheDocument();
-//   });
+    userEvent.type(searchBar, 'New York');
+     const suggestionItem = screen.getByText('New York, USA');
+     expect(suggestionItem).toBeInTheDocument();
+   });
 
-//   it('selects a city when clicking on a suggestion', () => {
-//     render(<App />);
+   it('selects a city when clicking on a suggestion', () => {
+     render(<App />);
 
-//     const searchBar = screen.getByPlaceholderText('Search cities...');
+     const searchBar = screen.getByPlaceholderText('Search cities...');
 
-//     userEvent.type(searchBar, 'Paris');
-//     const suggestionItem = screen.getByText('Paris, France');
-//     userEvent.click(suggestionItem);
+     userEvent.type(searchBar, 'Paris');
+     const suggestionItem = screen.getByText('Paris, France');
+     userEvent.click(suggestionItem);
 
-//     const selectedCity = screen.getByText('Selected City: Paris, France');
-//     expect(selectedCity).toBeInTheDocument();
-//   });
-// });
+     const selectedCity = screen.getByText('Selected City: Paris, France');
+     expect(selectedCity).toBeInTheDocument();
+   });
+});
